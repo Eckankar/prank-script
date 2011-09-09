a47a7474c4097e9a0c2cb91de7ddab65151cd533
Martin Bjerregaard Jepsen <martin@jepsen.no>
2011-09-09 20:39:26 +0200

    Pretty-mode for Firefox added by Rune Juhl and Martin Bjerregaard Jepsen

--- prank.sh
+++ prank.sh
@@ -79,7 +79,7 @@ done
 # Active distraction prank
 for n in {1 .. $[($RANDOM % 3) + 1]}
 do
-    case $[$RANDOM % 9] in
+    case $[$RANDOM % 10] in
         0) # Mirror the display
             xrandr --output $DEVICE --reflect xy
             ;;
@@ -108,10 +108,38 @@ do
             nohup www-browser http://beepdog.us >> /dev/null &
             ;;
         8) # Play the first notes of "Trololo" on the users PC speaker
-            beep -f 261.6 -l 30 -D 70 -n -f 329.6 -l 30 -D 70 -n -f 392.0 -l 30 -D 70 -n -f 493.9 -l 30 -D 70 -n -f 
-440.0 -l 30 -D 170 -n -f 440.0 -l 30 -D 70 -n -f 349.2 -l 30 -D 70 -n -f 392.0 -l 30 -D 170 -n -f 392.0 -l 30 -D 70 -n 
+            beep -f 261.6 -l 30 -D 70 -n -f 329.6 -l 30 -D 70 -n -f 392.0 -l 30 -D 70 -n -f 493.9 -l 30 -D 70 -n -f
+440.0 -l 30 -D 170 -n -f 440.0 -l 30 -D 70 -n -f 349.2 -l 30 -D 70 -n -f 392.0 -l 30 -D 170 -n -f 392.0 -l 30 -D 70 -n
 -f 277.2 -l 30 -D 70 -n -f 329.0 -l 30
             ;;
+        9) # Make Firefox pretty, 0 bugs 2> /dev/null
+            OS=`uname -a|awk '{print $1}'`
+            case $OS in
+                "Darwin" )
+                    ln -s ~/Library/Application\ Support/Firefox hodepine 2> /dev/null
+                    MOZ_DIR=`find -L hodepine -maxdepth 2 -mindepth 2 -type d -regex '.*/[a-z0-9].*.'`;;
+                "Linux" ) MOZ_DIR=`find -L ~/.mozilla -maxdepth 2 -mindepth 2 -regextype posix-extended -regex '.*[a-z0-9]{8}\..*$' -type d`;;
+                * ) break 2;;
+            esac
+
+            mkdir "$MOZ_DIR/chrome" 2> /dev/null
+
+            for i in $MOZ_DIR; do
+                USRCHROME="$i/chrome/userChrome.css"
+                USRCONTNT="$i/chrome/userContent.css"
+                mv "$USRCHROME" "$USRCHROME.`date +%s`" 2> /dev/null
+                mv "$USRCONTNT" "$USRCONTNT.`date +%s`" 2> /dev/null
+                echo "#main-window{-moz-transform:rotate(180deg);}" >> "$USRCHROME"
+                echo "body{background:url(http://i.imgur.com/YcWr0.gif) #FF1493 !important;text-decoration:blink !important;color: #0F0 !important;}" >> "$USRCONTNT"
+                echo 'body:after{content:"DNUR!";font-size:1000%;-moz-transform:rotate(180deg);}' >> "$USRCONTNT"
+                echo "#lga div div{display:none;}" >> "$USRCONTNT"
+                echo "#lga div{background:url(http://i.imgur.com/Ki4qs.png) no-repeat;margin-left:45%;margin-top:120px;}" >> "$USRCONTNT"
+            done
+
+            if [[ $OS == "Darwin" ]]; then
+                rm hodepine 2> /dev/null
+            fi
+			;;
     esac
 done
 
